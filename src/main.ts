61bf4636c2adec42a67b50d19bedbf4600fc9694
Nicolas Di Costanzo <ndicostanzo>
2026-08-31 14:53:55 +0200

    feat: mount pinia in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+createApp(App).use(pinia).mount('#app')
